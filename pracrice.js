f177a470897c05bccc669af5e02195a330730a12
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-11 23:36:36 +0600

    first commit

---
+++ pracrice.js
@@ -0,0 +1,27 @@
+/* 
+* JS concepts
+? null vs udefined
+? == vs ===
+? scope: block, function, global
+? closure
+? hoisting
+? callback function
+? arguments. what is array like objects--> array vs array like object
+? truthy vs falsy value
+? ! vs !!
+*/
+
+/* 
+* Dev Tool
+? Elements tab
+? console tab
+? source tab
+? network tab
+? applicaiton
+? check responsive
+? Shortcut Keys
+*/
+
+/* 
+* nosto bank
+*/
